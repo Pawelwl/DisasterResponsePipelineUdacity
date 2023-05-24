417190f59e821a609f2fdce48b164d7b65d29ec0
Pawelwl <Pawelwl@users.noreply.github.com>
2023-05-24 21:39:31 +0200

    Update: adding docstrings to process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -3,12 +3,31 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    '''
+    Load data from two csv files and merge into a single DataFrame
+    
+    Input:
+    messages_filepath: filepath to messages csv file
+    categories_filepath: filepath to categories csv file
+    
+    Returns:
+    df: DataFrame merging messages and categories
+    '''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages, categories, on='id', how='left')
     return df
     
 def clean_data(df):
+    '''
+    Cleans the part of dataset related ot categories. Splits one 'categories' column into separate columns each representing one category.
+    
+    Input:
+    df: input DataFrame to be cleaned
+    
+    Returns:
+    df: output cleaned DataFrame
+    '''
     categories = df['categories'].str.split(';', expand=True)
     row = categories.iloc[0]
     category_colnames = row.apply(lambda x: x[:-2])
@@ -29,6 +48,13 @@ def clean_data(df):
     return df
 
 def save_data(df, database_filename):
+    '''
+    Creates a DataBase and saves 'df' DataFrame in it.
+    
+    Input:
+    df: input data (DataFrame)
+    database_filename: name od DataBase to be created
+    '''
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('MergedDataSet', engine, if_exists='replace', index=False) 
 
